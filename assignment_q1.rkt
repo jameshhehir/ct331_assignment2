265e31a6d446a4ba2b507e83e90bbed4a43059f5
James Hehir <j.hehir1@nuigalway.ie>
2017-10-25 17:54:51 +0100

    Another change to q1

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,6 +1,6 @@
 #lang racket
-(cons 1 2)
-(cons 1(cons 2 3))
-(cons "abc"(cons 7 (cons 1(cons 2 3))))
+(cons 1 (cons 2 empty))
+(cons 1(cons 2 (cons 3 empty)))
+(cons "abc"(cons 7 (cons 1(cons 2 (cons 3 empty)))))
 (list "abc" '11 (list 1 2 3))
 (append '(abc) '(11) '( 1 2 3))
