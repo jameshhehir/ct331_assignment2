41e6b073c571b7d70af1b41b7096da1c925015e4
James Hehir <j.hehir1@nuigalway.ie>
2017-10-26 19:25:49 +0100

    FInished q2

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,16 +1,5 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
 ;A
 (define (ins_beg a b)         
   (cons a b))                 
@@ -26,5 +15,37 @@
 (ins_end '(a b) '(b c d))
 
 ;C
+(define (cout_top_level tree)
+  (if (or (not (list? tree)) (null? tree)) 0         
+      (+ 1 (apply max (map cout_top_level tree)))))  
 
+(cout_top_level '(1 2 3(4 5 6)))
 
+;D
+(define (count_instances key lst)
+   (if (null? lst)                  
+       0
+       (if (eq? key (car lst))      
+           (+ 1 (count_instances key (cdr lst)))
+           (count_instances key (cdr lst)))))
+
+(count_instances 7 '(4 4 5 7 7 6 7 9 7))
+
+;E
+(define (count_instances_tr key lst) 			
+  (let total ((lst lst) (n 0))
+    (if (null? lst)
+        n
+        (total (cdr lst)
+                  (if (eq? key (car lst))
+                      (+ n 1)
+                      n)))))
+(count_instances_tr 7 '(4 4 5 7 7 6 7 9 7))
+
+;F
+(define (count_instances_deep lst)
+  (cond ((null? lst) 0)
+        ((list? lst) (+ (count_instances_deep (car lst)) (count_instances_deep (cdr lst))))
+        (else 1)))
+
+(count_instances_deep '((2 2) 7 (3 4)))
